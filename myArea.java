ff1eb76a120c805154133934c856f2125a1cc6fe
Michael A. Rupert <michael.a.rupert@gmail.com>
2017-07-09 10:03:52 -0400

    Update myArea.java

--- myArea.java
+++ myArea.java
@@ -1,54 +1,75 @@
 package myArea;
+
 import javax.swing.JOptionPane;
 
-public class myArea {
+public class myArea2 {
+	private static final int parsedMeasure = 0;
+	public static String askMeasure;
 
-	public static void main(String[] args) {
-		int askWidth = 0;
-		int askHeight = 0;
-		JOptionPane.showMessageDialog(null, "We are going to Compute the area of a rectangle");
-		//boolean valid = false;
-		boolean validW = false;
-		boolean validH = false;
-		while (!validW || !validH) {
-		//while (!valid) {
+	public myArea2() {
+	}
+	public int userData(int askMeasure) {
+		return askMeasure;
+	}
+	public int userInput2() {
+		return Integer.parseInt(askMeasure);
+	}
+	public int userInput1() {
+		return Integer.parseInt(askMeasure);
+	}
 
+	// ******User Input Class with error handling ******
+	public int userData() {
+
+		boolean validM = false;
+		while (!validM) {
 			try {
-				askWidth = Integer.parseInt(JOptionPane.showInputDialog("Enter The Width"));
-				if (askWidth >= 0)
-					//valid = true;
-					validW = true;
-			} 
+				String askMeasure = JOptionPane.showInputDialog(null, "Enter the measurement");
+				//askMeasure = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter the measurement"));
+					if (askMeasure == null) { JOptionPane.showMessageDialog(null, "You pressed cancel, exiting program now!"); System.exit(0); }
+					
+					if (Integer.parseInt(askMeasure) >= 0) {
+					validM = true;
+					int parsedMeasure = Integer.parseInt(askMeasure);
+					return parsedMeasure;
+
+					}
+					
+			}
+
 			catch (NumberFormatException e) {
-				JOptionPane.showInputDialog("That's not a number. Please try again.");
+				JOptionPane.showMessageDialog(null, "That's not a number. Please try again.",null, JOptionPane.ERROR_MESSAGE);
 			}
-
-			try {
-				askHeight = Integer.parseInt(JOptionPane.showInputDialog("Enter The Height"));
-				if (askHeight >= 0)
-					//valid = true;
-					validH = true;
-			} 
-			catch (NumberFormatException e) {
-				JOptionPane.showInputDialog("That's not a number. Please try again.");
-			}
-
-			if (askWidth == askHeight) {
-
-				JOptionPane.showMessageDialog(null, "The shape is a square");
-
-			}
-
-			JOptionPane.showMessageDialog(null, "The Area of the Rectangle is " + askWidth * askHeight, null, JOptionPane.INFORMATION_MESSAGE);
+	 
 		}
+		return parsedMeasure;
+	}
+
+	// ********Main Method*******
+	public static void main(String[] args) {
+
+		JOptionPane.showMessageDialog(null, "We are going to Compute the area of a rectangle");
+		myArea2 userInput1 = new myArea2();
+		//userInput1.userData();
+		int parsedMeasure = userInput1.userData();
+		int length = userInput1.userData(parsedMeasure);
+
+		myArea2 userInput2 = new myArea2();
+		//userInput2.userData();
+		int parsedMeasure2 = userInput2.userData();
+		int width = userInput2.userData(parsedMeasure2);
+		
+		int area = length * width;
+		
+		if (width == length) {
+
+			JOptionPane.showMessageDialog(null, "The shape is a square");
+
+		}
+		
+		JOptionPane.showMessageDialog(null, "The Area of the Rectangle is " + area, null,
+				JOptionPane.INFORMATION_MESSAGE);
 
 	}
 
-	
-	/*
-	 * if (askHeight == null) { JOptionPane.showMessageDialog(null,
-	 * "User pressed cancel, exiting program now!"); System.exit(0); }
-	 */
-	
 }
-
